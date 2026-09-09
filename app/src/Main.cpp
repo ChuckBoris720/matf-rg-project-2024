36e23955713eadae8331b88d7d8a34eb8939b029
BorisAndjelkovic_mi23084 <chuckboris720@gmail.com>
2026-09-09 14:12:38 +0200

    Implement camera controls

--- app/src/Main.cpp
+++ app/src/Main.cpp
@@ -6,12 +6,44 @@
 class SceneController : public engine::core::Controller {
 
 public:
+    void initialize() override;
+
+    void update() override;
+
     void begin_draw() override;
 
     void draw() override;
 
     void end_draw() override;
 };
+
+void SceneController::initialize() {
+    engine::graphics::OpenGL::enable_depth_testing();
+
+    auto camera = engine::core::Controller::get<engine::graphics::GraphicsController>()->camera();
+    camera->Position = glm::vec3(0.0f, 2.0f, 5.0f);
+}
+
+void SceneController::update() {
+    auto platform = engine::core::Controller::get<engine::platform::PlatformController>();
+    auto camera = engine::core::Controller::get<engine::graphics::GraphicsController>()->camera();
+
+    float dt = platform->dt();
+
+    if (platform->key(engine::platform::KEY_W).is_down()) camera->move_camera(engine::graphics::Camera::Movement::FORWARD, dt);
+
+    if (platform->key(engine::platform::KEY_A).is_down()) camera->move_camera(engine::graphics::Camera::Movement::LEFT, dt);
+
+    if (platform->key(engine::platform::KEY_S).is_down()) camera->move_camera(engine::graphics::Camera::Movement::BACKWARD, dt);
+
+    if (platform->key(engine::platform::KEY_D).is_down()) camera->move_camera(engine::graphics::Camera::Movement::RIGHT, dt);
+
+
+    auto mouse = platform->mouse();
+
+    camera->rotate_camera(mouse.dx, mouse.dy);
+    camera->zoom(mouse.scroll);
+}
 
 void SceneController::begin_draw() { engine::graphics::OpenGL::clear_buffers(); }
 
@@ -23,7 +55,6 @@ void SceneController::draw() {
     auto shader = resources->shader("basic");
 
     auto camera = graphics->camera();
-    camera->Position = glm::vec3(3.0f, 2.0f, 5.0f);
 
     auto view = camera->view_matrix();
     auto projection = graphics->projection_matrix();
